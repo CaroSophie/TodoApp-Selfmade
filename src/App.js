3a79ed2e502557c9c309bf60f3431567db08b3aa
Caroline Jörgens <caroline.jorgens@aol.com>
2018-11-08 22:16:53 +0100

    First story input and written list array

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react'
 import './App.css'
+import Input from './Input'
+import Todo from './Todo'
 
 class App extends Component {
+  todos = ['Geschenke', 'Deko', 'Silvester planen']
+
   render() {
-    return <div> Last try</div>
+    return (
+      <div className="App">
+        <Input />
+        <div>
+          {this.todos.map(todo => (
+            <Todo text={todo} />
+          ))}
+        </div>
+      </div>
+    )
   }
 }
 
