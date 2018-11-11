65d9a51e89e157a43656cb4956e441c2d8b6dfa0
Caroline Jörgens <caroline.jorgens@aol.com>
2018-11-11 13:50:33 +0100

    Deletebutton and styling

--- src/App.js
+++ src/App.js
@@ -2,22 +2,11 @@ import React, { Component } from 'react'
 import './App.css'
 import Input from './Input'
 import Todo from './Todo'
+import Delete from './Delete'
 
 class App extends Component {
   state = {
     todos: []
-  }
-
-  toggleDone = index => {
-    const { todos } = this.state
-    const toggleDone = [
-      ...todos.slice(0, index),
-      { ...todos[index], done: !todos[index].done },
-      ...todos.slice(index + 1)
-    ]
-    this.setState({
-      todos: toggleDone
-    })
   }
 
   addTodoArray = event => {
@@ -33,22 +22,54 @@ class App extends Component {
     }
   }
 
+  toggleDone = index => {
+    const { todos } = this.state
+    const toggleDone = [
+      ...todos.slice(0, index),
+      { ...todos[index], done: !todos[index].done },
+      ...todos.slice(index + 1)
+    ]
+    this.setState({
+      todos: toggleDone
+    })
+  }
+
+  deleteToDo = index => {
+    const { todos } = this.state
+    const deleteTodos = [...todos.slice(0, index), ...todos.slice(index + 1)]
+
+    this.setState({
+      todos: deleteTodos
+    })
+  }
+
+  renderTodoList() {
+    return this.state.todos.map((todo, index) => (
+      <div className="Showtodos">
+        <Todo
+          key={todo.text}
+          text={todo.text}
+          toggle={() => {
+            this.toggleDone(index)
+          }}
+          done={todo.done}
+        />
+        <Delete
+          deletefunction={() => {
+            this.deleteToDo(index)
+          }}
+        />
+      </div>
+    ))
+  }
+
   render() {
     return (
-      <div className="App">
-        <Input keyupfunction={this.addTodoArray} />
-        <div>
-          {this.state.todos.map((todo, index) => (
-            <Todo
-              key={todo.text}
-              text={todo.text}
-              toggle={() => {
-                this.toggleDone(index)
-              }}
-              done={todo.done}
-            />
-          ))}
+      <div>
+        <div className="Styleinput">
+          <Input keyupfunction={this.addTodoArray} />{' '}
         </div>
+        {this.renderTodoList()}
       </div>
     )
   }
--- src/Input.js
+++ src/Input.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import './Input.css'
 
 class Input extends Component {
   render() {
     const { keyupfunction } = this.props
     return (
       <input
+        className="Input"
         type="text"
         placeholder="Add todo here"
         onKeyUp={event => {
