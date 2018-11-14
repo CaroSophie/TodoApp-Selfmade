e97aeec9011f31a13174521ccb77d00aab2e682c
Caroline Jörgens <caroline.jorgens@aol.com>
2018-11-14 17:57:16 +0100

    Add knobs

---
+++ docs/precache-manifest.bcdfdcc31b1f94561377c083ca0a6bd2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8892e87bdd58aedcecae",
+    "url": "/static/js/main.8892e87b.chunk.js"
+  },
+  {
+    "revision": "b35cca3c274f29be9238",
+    "url": "/static/js/1.b35cca3c.chunk.js"
+  },
+  {
+    "revision": "38a6380deca1d47f4d744aea1ff4c3b5",
+    "url": "/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c9486d8a23197d38b122a247cad892f.js"
+  "/precache-manifest.bcdfdcc31b1f94561377c083ca0a6bd2.js"
 );
 
 workbox.clientsClaim();
