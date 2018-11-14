201dd7386ad08649c6d089ac399072205fcbc156
Caroline Jörgens <caroline.jorgens@aol.com>
2018-11-14 17:30:47 +0100

    Knobs

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs'
+
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 
@@ -12,35 +14,42 @@ import Input from '../Input'
 import Separator from '../Separator'
 
 storiesOf('Todo', module)
+  .addDecorator(withKnobs)
   .add('TODO', () => (
     <Todo text="Test" done={false} toggle={action('toggled')} />
   ))
+  .addDecorator(withKnobs)
   .add('DONE', () => (
     <Todo text="Test" done={true} toggle={action('toggled')} />
   ))
 
-storiesOf('Progressbar', module).add('Show percents', () => (
-  <Progressbar percentage={0.3} />
-))
+storiesOf('Progressbar', module)
+  .addDecorator(withKnobs)
+  .add('Show percents', () => <Progressbar percentage={0.3} />)
 
-storiesOf('Button', module).add('Togglebutton', () => (
-  <Button
-    defaultText={'Just kidding'}
-    alternativeText={'Click here'}
-    onClick={() => console.log('click')}
-  />
-))
+storiesOf('Button', module)
+  .addDecorator(withKnobs)
+  .add('Togglebutton', () => (
+    <Button
+      defaultText={'Just kidding'}
+      alternativeText={'Click here'}
+      onClick={() => console.log('click')}
+    />
+  ))
 
-storiesOf('Counter', module).add('Counting', () => <Counter num={1} />)
+storiesOf('Counter', module)
+  .addDecorator(withKnobs)
+  .add('Counting', () => <Counter num={1} />)
 
-storiesOf('Delete', module).add('Deletebutton', () => (
-  <Delete handleDelete={action('delete Todo')} />
-))
+storiesOf('Delete', module)
+  .addDecorator(withKnobs)
+  .add('Deletebutton', () => <Delete handleDelete={action('delete Todo')} />)
 
-storiesOf('Input', module).add('Press enter', () => (
-  <Input onEnter={action('Show written input')} />
-))
+storiesOf('Input', module)
+  .addDecorator(withKnobs)
+  .add('Press enter', () => <Input onEnter={action('Show written input')} />)
 
 storiesOf('Separator', module)
+  .addDecorator(withKnobs)
   .add('TODO', () => <Separator text="TODO" />)
   .add('DONE', () => <Separator text="DONE" />)
